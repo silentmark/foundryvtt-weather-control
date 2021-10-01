bc3502bbff580297a4b660d4bbe06fc3e5349e03
eCyclops <github@ecyclops.com>
2021-09-30 21:48:03 -0400

    Return scripts/install-script.sh to its rightful place.

---
+++ scripts/install-script.sh
@@ -0,0 +1,12 @@
+sleep 1
+# Set permissions.
+mkdir -p /foundry/fvtt /foundry/data
+chown 1000:100 -R /foundry
+chmod 776 -R /foundry
+chmod +x /foundry/
+# Run.
+echo "INFO ! Starting FoundryVTT Server"
+echo " "
+# exec node /foundry/fvtt/resources/app/main.js --dataPath=/foundry/data
+su foundry -c 'node /foundry/fvtt/resources/app/main.js --dataPath=/foundry/data'
+exit
